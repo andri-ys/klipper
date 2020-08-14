080ebc17704f08a7013175d248f3f8fc091f58d7
Andri YS <andri.ys.st@gmail.com>
2020-08-14 15:05:13 +0700

    remove commented code

--- klippy/extras/delta_calibrate.py
+++ klippy/extras/delta_calibrate.py
@@ -90,16 +90,13 @@ class DeltaCalibrate:
         # Calculate default probing points
         radius = config.getfloat('radius', above=0.)
         points = [(0., 0.)]
-        #scatter = [.95, .90, .85, .70, .75, .80]
         
         extra_count = config.getint('samples', 6, minval=2)
         
         for i in range(extra_count):
             r = math.radians(90. + (360. / extra_count) * i)
-            dist = radius # * scatter[i]
+            dist = radius
             points.append((math.cos(r) * dist, math.sin(r) * dist))
-            #dist = dist * .50
-            #points.append((math.cos(r) * dist, math.sin(r) * dist))
         self.probe_helper = probe.ProbePointsHelper(
             config, self.probe_finalize, default_points=points)
         self.probe_helper.minimum_points(3)
@@ -249,7 +246,6 @@ class DeltaCalibrate:
         self.gcode.respond_info("probe count : %d" % (count))
         self.gcode.respond_info("average : %.6f" % (avg))
         self.gcode.respond_info("stdev : %.6f" % (sd))
-        # self.gcode.respond_info("probe result : %.6f" % (self.probe_helper.results[0][2]))
     def add_manual_height(self, height):
         # Determine current location of toolhead
         toolhead = self.printer.lookup_object('toolhead')
